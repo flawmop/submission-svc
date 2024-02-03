50b748e02a4bf98e99beda2cfdc74e8bd47d1596
flawmop <flawmop@insilicosoft.com>
2024-02-02 23:39:02 -0800

    Place debug logging in application.yml. Add controller mapping

--- src/main/java/com/insilicosoft/portal/ripsvr/controller/FileAsyncUploadController.java
+++ src/main/java/com/insilicosoft/portal/ripsvr/controller/FileAsyncUploadController.java
@@ -6,17 +6,25 @@ import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.insilicosoft.portal.ripsvr.service.InputProcessorService;
 
 @Controller
+@RequestMapping("/run")
 public class FileAsyncUploadController {
 
   @Autowired
   InputProcessorService inputProcessorService;
+
+  @GetMapping()
+  public CompletableFuture<ResponseEntity<String>> get() {
+    return CompletableFuture.completedFuture(ResponseEntity.ok("All good!!"));
+  }
 
   @PostMapping("/uploadAsync")
   public CompletableFuture<ResponseEntity<String>> handleFileUpload(final @RequestParam("file")
